fda3254792733ec3dc32fcf16032f66dcdbeeb06
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-02-06 21:59:48 +0100

    fixing issue where account details is not well rendered on small devices

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,4 +1,5 @@
 from accounts.models import Account
+from accounts import constants
 import logging
 logger = logging.getLogger(__name__)
 
@@ -11,6 +12,7 @@ def account_context(request):
         try:
             account = Account.objects.get(user=request.user)
             context['account'] = account
+            context['ACCOUNT_TYPE'] : constants.ACCOUNT_TYPE
         except Account.DoesNotExist :
             logging.warning(f"No account found for user {request.user.username}")
         
