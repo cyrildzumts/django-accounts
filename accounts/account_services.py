cb555155626843885c2809af1048387f31e3647a
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-02-19 18:20:05 +0100

    mi o rupdate

--- accounts/account_services.py
+++ accounts/account_services.py
@@ -13,6 +13,7 @@ from django.apps import apps
 from django.forms import modelform_factory
 from django.utils import timezone
 from django.core.mail import send_mail
+from django.conf import settings
 from django.template.loader import render_to_string
 import sys
 import logging
