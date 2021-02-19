018ed0d2f08b8528fc62370f2cd1697fca7b33df
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-02-19 12:23:20 +0100

    minor update

--- accounts/tokens_gen.py
+++ accounts/tokens_gen.py
@@ -1,17 +1,9 @@
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils import six, timezone
+
+from django.utils import timezone
 from accounts import constants
 import secrets
 import datetime
 
-class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
-    def _make_hash_value(self, user, timestamp):
-        return (
-            six.text_type(user.pk) + six.text_type(timestamp) +
-            six.text_type(user.profile.email_confirmed)
-        )
-
-account_activation_token = AccountActivationTokenGenerator()
 
 def get_activation_token():
     return secrets.token_urlsafe(constants.TOKEN_LENGTH)
